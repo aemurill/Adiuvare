4c40aa4cc262de1af2834f1b678b5e2acc674387
Madeline Conner <maddyjconner@gmail.com>
2017-11-12 16:05:33 -0800

    Worked on the audio transcription, finding that the request code is not REQUEST_OK and the data is null..

--- app/src/main/java/edu/ucsc/cmps121/adiuvare/ListeningScreen.java
+++ app/src/main/java/edu/ucsc/cmps121/adiuvare/ListeningScreen.java
@@ -3,7 +3,9 @@ package edu.ucsc.cmps121.adiuvare;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Build;
+import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,48 +13,56 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Locale;
 
+import static android.speech.RecognizerIntent.getVoiceDetailsIntent;
+import static android.speech.SpeechRecognizer.createSpeechRecognizer;
 import static android.speech.SpeechRecognizer.isRecognitionAvailable;
 
 public class ListeningScreen extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listening_screen);
 
-        // what do we want to happen when someone clicks on the headphones (currently an ear)? here is a first attempt
+        // code to execute when someone selects the ear
         findViewById(R.id.listenButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // do we want a new speech recognizer object each time?
-                // not sure if this works, but should check to see if this is available on their android system
                 boolean available = isRecognitionAvailable(ListeningScreen.this);
                 if (available) {
-                    // then try to recognize speech
-                    Toast.makeText(ListeningScreen.this, "Trying to listen!", Toast.LENGTH_SHORT).show();
+                    // then notify & try to recognize speech
+                    Toast.makeText(ListeningScreen.this, "Begin Listening", Toast.LENGTH_SHORT).show();
+                    listen();
                 } else {
                     Toast.makeText(ListeningScreen.this, "Sorry, your phone does not have speech recognition capabilities", Toast.LENGTH_LONG).show();
                 }
-                listen(); //<- this was a suggested method from an online tutorial
             }
         });
 
     }
 
     private void listen(){
+        //SpeechRecognizer listening = createSpeechRecognizer(this);
+        //RecognitionListener test = listening.getVoiceDetailsIntent(this);
+        //listening.startListening(test);
         Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        i.putExtra(RecognizerIntent.EXTRA_PROMPT, "Listening");
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US"); // for english only
+        //i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        //i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        //i.putExtra(RecognizerIntent.EXTRA_PROMPT, "Listening");
 
         try {
-            startActivityForResult(i, 100);
+            startActivityForResult(i, 1);
+            //displayResults.setText("");
         } catch (ActivityNotFoundException a) {
             Toast.makeText(ListeningScreen.this, "Your device doesn't support Speech Recognition", Toast.LENGTH_SHORT).show();
         }
@@ -62,11 +72,20 @@ public class ListeningScreen extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         Toast.makeText(ListeningScreen.this, "Results Received", Toast.LENGTH_SHORT).show();
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 100){
-            if (resultCode == RESULT_OK && data != null) {
+        if(requestCode == 1){
+            Toast.makeText(ListeningScreen.this, "FOUND RQC 1", Toast.LENGTH_SHORT).show();
+            if (null != data){ // not result_ok
+                Toast.makeText(ListeningScreen.this, "Data is not null", Toast.LENGTH_SHORT).show();
+            }else if (resultCode == RESULT_OK && null != data) {
                 ArrayList<String> res = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                 String inSpeech = res.get(0);
-                recognition(inSpeech);
+                Log.e("Speech",""+inSpeech);
+                TextView displayResults = (TextView) findViewById(R.id.displayVocal);
+                displayResults.setText(inSpeech);
+
+            } else {
+                TextView displayResults = (TextView) findViewById(R.id.displayVocal);
+                displayResults.setText("Something didn't work");
             }
         }
     }
@@ -74,6 +93,8 @@ public class ListeningScreen extends AppCompatActivity {
     private void recognition(String text){
         Log.e("Speech",""+text);
     }
+
+    // if voice recognition begins, display cancel recognition symbol and then execute .cancel() method
 
 
 }
